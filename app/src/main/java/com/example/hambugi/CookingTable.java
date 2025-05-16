d3e04d7df92a5b389539f883c33ccd33e1ad37ce
kmj54 <kmj549735@gmail.com>
2025-05-16 16:34:42 +0900

    feat: end_day layout & button image change & change view between counter & cookingtable

--- app/src/main/java/com/example/hambugi/CookingTable.java
+++ app/src/main/java/com/example/hambugi/CookingTable.java
@@ -1,4 +1,53 @@
 package com.example.hambugi;
 
-public class CookingTable {
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
+
+import androidx.annotation.Nullable;
+
+
+public class CookingTable extends Activity {
+    private TextView txt_menubar_time, txt_menubar_gold;
+    private ImageButton btn_delete, btn_complete, btn_change_view, btn_menu;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.view_cookingtable);
+
+        txt_menubar_time = findViewById(R.id.txt_menubar_time);
+        txt_menubar_gold = findViewById(R.id.txt_menubar_gold);
+        btn_delete = findViewById(R.id.btn_delete);
+        btn_complete = findViewById(R.id.btn_complete);
+        btn_change_view = findViewById(R.id.btn_change_view);
+        btn_menu = findViewById(R.id.btn_menubar_menu);
+
+        // 화면 전환 버튼 클릭 시
+        btn_change_view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CookingTable.this, Counter.class);
+                startActivity(intent);
+            }
+        });
+
+        // 햄버거 삭제 버튼 클릭 시
+        btn_delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        // 햄버거 완성 버튼 클릭 시
+        btn_complete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
 }
--- app/src/main/java/com/example/hambugi/Counter.java
+++ app/src/main/java/com/example/hambugi/Counter.java
@@ -1,4 +1,35 @@
 package com.example.hambugi;
 
-public class Counter {
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Counter extends AppCompatActivity {
+    private TextView txt_menubar_time, txt_menubar_gold;
+    private ImageButton btn_change_view, btn_menu;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.view_counter);
+
+        txt_menubar_time = findViewById(R.id.txt_menubar_time);
+        txt_menubar_gold = findViewById(R.id.txt_menubar_gold);
+        btn_change_view = findViewById(R.id.btn_change_view);
+        btn_menu = findViewById(R.id.btn_menubar_menu);
+
+        // 화면 전환 버튼 클릭 시
+        btn_change_view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Counter.this, CookingTable.class);
+                startActivity(intent);
+            }
+        });
+    }
 }
